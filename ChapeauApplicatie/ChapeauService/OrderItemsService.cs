4d50155cca1cb767b3fb480e341605e86b8f0496
Itzjoey2003 <joeyberglund2003@gmail.com>
2022-05-19 12:59:16 +0200

    Commit Je kan er nu als boom doorheen lopen

---
+++ ChapeauApplicatie/ChapeauService/OrderItemsService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChapeauDAL;
+using ChapeauModels;
+
+namespace ChapeauService
+{
+    public class OrderItemsService
+    {
+        OrderItemsDao orderitemsdb;
+
+        public OrderItemsService()
+        {
+            orderitemsdb = new OrderItemsDao();
+        }
+
+        public List<OrderItems> GetOrderItems()
+        {
+            List<OrderItems> orderItems = orderitemsdb.GetAllOrderItems();
+            return orderItems;
+        }
+
+    }
+}
