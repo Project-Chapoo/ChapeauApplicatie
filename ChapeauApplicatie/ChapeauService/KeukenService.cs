86d1c4b5dbd027631f37604256139fef812cc142
ThijsMoerland <Moerland8@gmail.com>
2022-05-26 16:35:32 +0200

    KeukenForm laad orders

---
+++ ChapeauApplicatie/ChapeauService/KeukenService.cs
@@ -0,0 +1,32 @@
+﻿using ChapeauDAL;
+using ChapeauModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauService
+{
+    public class KeukenService
+    {
+        KeukenDAO keukenDAO;
+        public KeukenService()
+        {
+            keukenDAO = new KeukenDAO();
+        }
+        public List<KeukenItemModel> GetFirstOrder()
+        {
+            return keukenDAO.GetFirstOrder();
+        }
+        public List<KeukenItemModel> GetSecondOrder()
+        {
+            return keukenDAO.GetSecondOrder();
+        }
+        public List<KeukenItemModel> GetThirdOrder()
+        {
+            return keukenDAO.GetThirdOrder();
+        }
+    }
+
+}
---
+++ ChapeauApplicatie/UIChapeau/Forms/Payingfrm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChapeauUI.Forms
+{
+    public partial class Payingfrm : Form
+    {
+        private double tip;
+        
+        public Payingfrm()
+        {
+            InitializeComponent();
+        }
+
+        public Payingfrm(double tip)
+        {
+            InitializeComponent();
+            this.tip = tip;
+        }
+
+        private void btnAddTip_Click(object sender, EventArgs e)
+        {
+            Tipfrm tf = new Tipfrm(5);
+            this.Hide();
+            tf.Closed += (s, args) => this.Close();
+            tf.Show();
+        }
+    }
+}
