a9b4fdb0876d7ff8c1c6e8a64149454925731159
l-huisman <64755533+l-huisman@users.noreply.github.com>
2022-05-27 22:24:09 +0200

    Revert "Commit"

--- ChapeauApplicatie/ChapeauUI/Program.cs
+++ ChapeauApplicatie/ChapeauUI/Program.cs
@@ -1,4 +1,4 @@
-namespace ChapeauUI
+namespace MainForm
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace ChapeauUI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new PDAOrdering_Joey());
+            Application.Run(new Form1());
         }
     }
 }
